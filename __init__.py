0de3e826835b9fd40c25b988f594c83495e64148
Santodan <danny.santos93@hotmail.com>
2025-06-03 16:28:39 +0100

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,9 @@
+from .random_lora_node import CR_RandomLoRACustom
+
+NODE_CLASS_MAPPINGS = {
+    "RandomLoRACustom": RandomLoRACustom,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "RandomLoRACustom": "Random LoRA Selector",
+}
