9636c356e790ee98088aadbf04cee50a1c6e3afe
Santodan <danny.santos93@hotmail.com>
2025-06-07 19:15:38 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,10 +2,12 @@ from .santodan_nodes.random_lora_node import *
 
 NODE_CLASS_MAPPINGS = {
     "RandomLoRACustom": RandomLoRACustom,
+    "RandomLoRAFolder": RandomLoRAFolder,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "RandomLoRACustom": "Random LoRA Selector",
+    "RandomLoRAFolder": "Random LoRA Folder Selector",
 }
 
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
